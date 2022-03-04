69aee3f6464bb0cd84117cecad11f6d89161f547
Orla Fletcher-Dowd <t-orflet@microsoft.com>
2022-03-04 14:04:41 +0000

    registered Engagement model with Admin Panel

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Task
+from .models import Engagement
 # Register your models here.
 
+admin.site.register(Engagement) #registers model with admin panel
 admin.site.register(Task) #registers model with admin panel
