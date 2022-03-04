df6776ee67424319c93e943da002d78c83b919a5
Orla Fletcher-Dowd <t-orflet@microsoft.com>
2022-03-04 13:55:24 +0000

    created Engagement model and migrated to DB

--- base/models.py
+++ base/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Engagement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    engagement = models.ForeignKey(Engagement, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
