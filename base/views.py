db53e10fbe4fe2eaf5ac3824c448c3ac845fe164
Orla Fletcher-Dowd <t-orflet@microsoft.com>
2022-03-07 11:34:36 +0000

    created logic for displaying engmt tasks

--- base/views.py
+++ base/views.py
@@ -50,20 +50,21 @@ class EngagementList(ListView):
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
             context['engagements'] = context['engagements'].filter(
-                title__icontains=search_input)
+                name__icontains=search_input)
 
         context['search_input'] = search_input
 
         return context
 
 class TaskList(LoginRequiredMixin, ListView):
-    model = Task
-    context_object_name = 'tasks' #looks for _task_list template
+    model = Task #looks for _task_list template
+    context_object_name = 'tasks' 
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+        
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
