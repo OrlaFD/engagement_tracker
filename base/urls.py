87c36cba05d0f7416127de4a18e42c67868fdca3
Orla Fletcher-Dowd <t-orflet@microsoft.com>
2022-03-28 09:53:40 +0100

    commits to main

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage, EngagementList
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -7,9 +7,11 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 
-    path('', TaskList.as_view(), name='tasks'),
+    path('', EngagementList.as_view(), name='engagements'),
+    path('tasks', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
+
