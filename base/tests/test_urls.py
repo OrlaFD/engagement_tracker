e9895be759dccb9131ce5b49a23211367e371a63
Orla Fletcher-Dowd <79976937+OrlaFD@users.noreply.github.com>
2022-03-30 13:43:27 +0100

    created tests for urls

--- base/tests/test_urls.py
+++ base/tests/test_urls.py
@@ -1,8 +1,32 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from base.views import EngagementList, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+
 
 class TestUrls(SimpleTestCase):
 
-    def test_engagements_url_is_resolved(self):
-        assert 1 == 2
+    def test_engagements_url_resolves(self):
+        url = reverse('engagements')
+        self.assertEquals(resolve(url).func.view_class, EngagementList)
+    
+    def test_tasks_url_resolves(self):
+        url = reverse('tasks')
+        self.assertEquals(resolve(url).func.view_class, TaskList)
+
+    def test_task_url_resolves(self):
+        url = reverse('task', args=['1'])
+        self.assertEquals(resolve(url).func.view_class, TaskDetail)
+
+    def test_taskcreate_url_resolves(self):
+        url = reverse('task-create')
+        self.assertEquals(resolve(url).func.view_class, TaskCreate)
+
+    def test_taskupdate_url_resolves(self):
+        url = reverse('task-update', args=['1'])
+        self.assertEquals(resolve(url).func.view_class, TaskUpdate)
+
+    def test_taskdelete_url_resolves(self):
+        url = reverse('task-delete', args=['1'])
+        self.assertEquals(resolve(url).func.view_class, DeleteView)
 
         
