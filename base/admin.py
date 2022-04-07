0667407e3efbb3ae913d2c7f2f4ab37e66f0fd0e
Orla Fletcher-Dowd <79976937+OrlaFD@users.noreply.github.com>
2022-04-07 09:51:35 +0100

    Updates

--- base/admin.py
+++ base/admin.py
@@ -3,5 +3,5 @@ from .models import Task
 from .models import Engagement
 # Register your models here.
 
-admin.site.register(Engagement) #registers model with admin panel
-admin.site.register(Task) #registers model with admin panel
+admin.site.register(Engagement) #registers Engagement model with admin panel
+admin.site.register(Task) #registers Task model with admin panel
--- engagement_tracker/urls.py
+++ engagement_tracker/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #admin interface (webwebweb)
+    path('admin/', admin.site.urls), #admin interface 
     path('', include('base.urls')),
 ]
